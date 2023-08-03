388ef7b62140195810c39e5d1607a3ee17691eb3
Vishal-Bharath-Kumar <vishalbharath.kanini@gmail.com>
2023-08-04 01:41:55 +0530

    update commit

--- ui/src/Components/User/User Feedback/UserFeedback.jsx
+++ ui/src/Components/User/User Feedback/UserFeedback.jsx
@@ -1,40 +1,53 @@
-import React ,{useRef}from 'react'
+import React, { useRef, useState, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
-import './UserFeedback.css'
+import './UserFeedback.css';
 
 const UserFeedback = () => {
-   
-    const form = useRef();
+  const form = useRef();
+  const [reloadFlag, setReloadFlag] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(
-        'service_updw4cq', 
-        'template_418pqmv',
-        form.current, 
-        'IkWbOj8hUcy1g8AWr'
-        )
-      .then((result) => {
+    emailjs
+      .sendForm('service_updw4cq', 'template_418pqmv', form.current, 'IkWbOj8hUcy1g8AWr')
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+          setReloadFlag(true);
+        },
+        (error) => {
           console.log(error.text);
-      });
-    }
-  return (
-    <div className='feedbackForm'>
-        <h1 className='cardHeading'>Share Your Feedback with Us!</h1>
-        <form ref={form} onSubmit={sendEmail}>
-        <label >Name</label>
-        <input type="text" name="user_name" />
-        <label >Email</label>
-        <input  type="email" name="user_email" />
-        <label >Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-        </form>
-    </div>
-  )
-}
+        }
+      );
+  };
 
-export default UserFeedback
+  useEffect(() => {
+    if (reloadFlag) {
+      // Reset the reloadFlag to false to prevent continuous reloading
+      setReloadFlag(false);
+
+      // Reload the component after 1 second (adjust the delay as needed)
+      setTimeout(() => {
+        window.location.reload();
+      }, 10);
+    }
+  }, [reloadFlag]);
+
+  return (
+    <div className="feedbackForm">
+      <h1 className="cardHeading">Share Your Feedback with Us!</h1>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" required />
+        <label>Email</label>
+        <input type="email" name="user_email" required />
+        <label>Message</label>
+        <textarea name="message" required />
+        <input type="submit" value="Send" />
+      </form>
+    </div>
+  );
+};
+
+export default UserFeedback;
